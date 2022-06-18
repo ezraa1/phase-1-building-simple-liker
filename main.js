72381fac675b93cbff351bc6fffdf3cfefe89da2
ezra kipchirchir <ezrakipchirchir1034@gmail.com>
2022-06-18 16:38:47 +0300

    Done

--- main.js
+++ main.js
@@ -4,6 +4,33 @@ const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
 
+document.addEventListener('DOMContentLoaded',()=>{
+  const likers = document.querySelectorAll('span.like-glyph')
+  likers.forEach(liker=>{
+    liker.addEventListener('click',()=>{
+      if (liker.innerText === FULL_HEART ) {
+        liker.classList.remove('activated-heart')
+        liker.innerText = EMPTY_HEART
+      }else{
+        mimicServerCall()
+        .then(()=>{
+          liker.innerText = FULL_HEART
+          liker.classList.add('activated-heart')
+        })
+        .catch(()=>{
+          let error = document.getElementById('modal')
+          error.classList.remove('hidden')
+          let message = document.getElementById('modal-message')
+          message.innerText = "Random server error. Try again."
+          setTimeout(()=>{
+            error.classList.add('hidden')
+          },3000)
+        })
+      }
+    })
+  })
+})
+
 
 
 
